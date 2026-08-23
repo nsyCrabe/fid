c6665dc22a6010bd5ad5ff64ead335f7da80e90f
nsyCrabe <mowmow59400@gmail.com>
2026-08-23 19:27:55 +0200

    Suppression couleur personnalisée et bulle noire

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cartes-fidelite-v7-color-wheel';
+const CACHE_NAME='cartes-fidelite-v8-no-custom-color';
 const ASSETS=['./','./index.html','./app.js','./style.css','./manifest.json','https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js','https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
